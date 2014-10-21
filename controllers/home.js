79eab3e8aa4c07b8da1ff8bd85e492d5daa6cc57
Daniel Wylie <daniel@danielwylie.me>
2014-10-21 18:33:59 +1300

    Now looks way better

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,17 @@
-exports.Setup = function(app){
-	app.get('/', this.home_get);
+exports.Setup = function (app) {
+    app.get('/', this.home_get);
+    app.get('/shnappy', this.shnappy_get);
+    app.get('/quizblaster', this.quizblaster_get);
 };
 
-exports.home_get = function(req, res){
-	res.render('home');
-}
+exports.home_get = function (req, res) {
+    res.render('home', {layout: null});
+};
+
+exports.shnappy_get = function (req, res) {
+    res.render('shnappy', {class: 'shnappy'});
+};
+
+exports.quizblaster_get = function (req, res) {
+    res.render('quizblaster', {class: 'quizblaster'});
+};
